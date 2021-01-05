89160dc8a6ece1e1fdf4a44e9795f7647c255442
Łukasz Drążewski <ldrazewski@future-processing.com>
2021-01-05 14:06:29 +0100

    Event for checking current theme

--- src/context/index.js
+++ src/context/index.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
-import { eventsToListen } from 'src/utils/events';
+import dispatchEvent, { eventsToListen, eventsToDispatch } from 'src/utils/events';
 import useWindowEventListener from 'src/hooks/useWindowEventListener';
 import { getThemeByName } from 'src/themes/themeSelector';
 
@@ -8,9 +8,21 @@ const CombinedContextProvider = ({ children }) => {
   const [activeThemeName, setActiveThemeName] = useState();
   const activeTheme = getThemeByName(activeThemeName);
 
-  const handleThemeChanged = e => setActiveThemeName(e.detail);
+  const handleThemeChanged = e => {
+    setActiveThemeName(e.detail);
+  }
 
   useWindowEventListener(eventsToListen.HOST_THEME_CHANGED, handleThemeChanged);
+
+  const checkCurrentTheme = () => {
+    console.log('event:', `${eventsToDispatch.HOST_THEME_CHECK}:` )
+    dispatchEvent(eventsToDispatch.HOST_THEME_CHECK);
+  }
+
+  useEffect(() => {
+    checkCurrentTheme();
+  }, []);
+
 
   return (
     <ThemeProvider theme={activeTheme}>
--- src/utils/events.js
+++ src/utils/events.js
@@ -2,6 +2,10 @@ export const eventsToListen = {
   HOST_THEME_CHANGED: 'HOST_THEME_CHANGED'
 };
 
+export const eventsToDispatch = {
+  HOST_THEME_CHECK: 'HOST_THEME_CHECK'
+}
+
 const dispatchEvent = (event, data) => window.dispatchEvent(new CustomEvent(event, { detail: data }));
 
 export default dispatchEvent;
