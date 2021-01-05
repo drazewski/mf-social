9a31731f96f0e3320a3e6fb80b4a425085a06d0e
Łukasz Drążewski <ldrazewski@future-processing.com>
2021-01-05 15:30:39 +0100

    HostApp imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Layout from './components/Layout';
-import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
-import CombinedContextProvider from './context';
-import TextComponent from './components/TextComponent';
 
-// We want to have prefixed class names
-const generateClassName = createGenerateClassName({
-  productionPrefix: 'mfSocial',
-  seed: 'mfSocial'
-});
+const HostApp = React.lazy(() => import("hostApp/App"));
 
-const SocialService = () => {
-  return (
-    <CombinedContextProvider>
-      <StylesProvider generateClassName={generateClassName}>
-        <Layout>
-          <Grid item xs={12}>
-            <TextComponent />
-          </Grid>
-        </Layout>
-      </StylesProvider>
-    </CombinedContextProvider>
-  );
-};
+const App = () => (
+  <React.Suspense fallback="Loading HostApp">
+    <HostApp  />
+  </React.Suspense>
+);
 
-export default SocialService;
+export default App;
